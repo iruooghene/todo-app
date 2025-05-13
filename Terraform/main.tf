8686418427aedd977d7d2ca6c7ef0b14c75b1fff
Victoria <ebohiruooghene@gmail.com>
2025-05-13 14:09:48 +0100

    new shege

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -8,37 +8,50 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_iam_role" "eks" {
-  name = "eks-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  role       = aws_iam_role.eks.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-
-# EKS Cluster (LocalStack has limited EKS support)
-resource "aws_eks_cluster" "local" {
-  name     = "todoapp-eks-local"
-  role_arn = aws_iam_role.eks.arn
-  vpc_config {
-    subnet_ids = [aws_subnet.main.id]
-  }
-}
-
+# Subnet
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+}
+
+# Security Group
+resource "aws_security_group" "main_sg" {
+  name        = "main-security-group"
+  description = "Allow all traffic (for testing)"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Route Table
+resource "aws_route_table" "main_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_igw.id
+  }
+}
+
+# Route Table Association
+resource "aws_route_table_association" "main_rta" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main_rt.id
 }
--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -7,6 +7,11 @@ terraform {
   }
 }
 
+provider "kubernetes" {
+  config_path = "/home/victoria/.kube/config"
+}
+
+
 provider "aws" {
   region                      = "us-east-1"
   skip_credentials_validation = true
